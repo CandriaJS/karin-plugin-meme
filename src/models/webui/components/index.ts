31d73fa953dfea7026f9b28f0dbc8eb5dbc914a7
wuliya <3369906077@qq.com>
2025-05-12 23:22:34 +0800

    refactor(webui): 优化 components 函数以提高性能
    - 将异步组件加载改为并行执行，以加快加载速度
    - 使用 Promise.all 替代单个异步调用，提高效率
    - 重构代码结构，提高可读性和维护性

--- src/models/webui/components/index.ts
+++ src/models/webui/components/index.ts
@@ -5,14 +5,18 @@ import { protectComponents } from './protect'
 import { serverComponents } from './server'
 import { statComponents } from './stat'
 
-export const components = async () => [
-  ...serverComponents(),
-  ...memeComponents(),
-  ...(await accessComponents()),
-  ...(await protectComponents()),
-  ...statComponents(),
-  ...otherComponents()
-]
+export const components = async () => {
+  const results = await Promise.all([
+    serverComponents(),
+    memeComponents(),
+    accessComponents(),
+    protectComponents(),
+    statComponents(),
+    otherComponents()
+  ])
+
+  return results.flat()
+}
 
 export {
   accessComponents,
