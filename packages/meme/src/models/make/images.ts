5d41794976b62cf591394d93ccd46a73afc47d88
wuliya <3369906077@qq.com>
2025-08-27 16:44:13 +0800

    refactor(meme): 调整用户头像处理顺序

--- packages/meme/src/models/make/images.ts
+++ packages/meme/src/models/make/images.ts
@@ -87,7 +87,7 @@ export async function handleImages(
       }
     }
 
-    userAvatars.push({
+    userAvatars.unshift({
       name: await utils.get_user_name(e, avatar.userId),
       data: avatar.image
     })
